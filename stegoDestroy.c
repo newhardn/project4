94472ca9cd0cd2ef61d0fe924f344973bbb6c9d2
nick newhard <newhardn@cwu.edu>
2023-11-09 12:27:13 -0800

    Added stub source and target build for stegDestroy

---
+++ stegoDestroy.c
@@ -0,0 +1,16 @@
+//
+// stegoDestroy --- insert info in a BMP image file
+// Created by nnewh on 11/9/2023.
+//
+// 4. Write a program, stegoDestroy.c that will destroy any information hidden in a file, assuming that the information
+// hiding method in stego.c might have been used. Your program should take a bmp file as input, and produce a bmp file
+// as output. Visually, the output file must be identical to the input file.
+//
+// 5. Test stegoDestroy.c on aliceStego.bmp. Verify that the output file image is undamaged. What information does
+// stego.c extract from your output file?
+//
+#include "stego.h"
+
+int main(int argc, const char *argv[]) {
+    return -1;
+}
